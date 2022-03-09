681d4d20d4ac1342e53e0da3997007499eb46a38
Tiago Leal <50256581+tiagofelli@users.noreply.github.com>
2022-03-09 00:37:35 -0300

    Update game.py

--- game.py
+++ game.py
@@ -4,11 +4,8 @@ import sys
 board=[i for i in range(0,9)]
 player, computer = '',''
 
-# Corners, Center and Others, respectively
 moves=((1,7,3,9),(5,),(2,4,6,8))
-# Winner combinations
 winners=((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
-# Table
 tab=range(1,10)
 
 def print_board():
@@ -60,22 +57,19 @@ def make_move(brd, player, move, undo=False):
         return (True, win)
     return (False, False)
 
-# AI goes here
+# A.I 
 def computer_move():
     move=-1
-    # If I can win, others don't matter.
     for i in range(1,10):
         if make_move(board, computer, i, True)[1]:
             move=i
             break
     if move == -1:
-        # If player can win, block him.
         for i in range(1,10):
             if make_move(board, player, i, True)[1]:
                 move=i
                 break
     if move == -1:
-        # Otherwise, try to take one of desired places.
         for tup in moves:
             for mv in tup:
                 if move == -1 and can_move(board, computer, mv):
